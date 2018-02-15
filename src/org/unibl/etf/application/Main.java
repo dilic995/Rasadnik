37e63b2178a5242d666af772d2c57621b4fa69ae
Unknown <dragan995ilic@gmail.com>
2018-02-15 13:38:40 +0100

    main

--- src/org/unibl/etf/application/Main.java
+++ src/org/unibl/etf/application/Main.java
@@ -2,7 +2,6 @@ package org.unibl.etf.application;
 
 import java.io.IOException;
 
-import org.unibl.etf.dao.interfaces.DAOFactory;
 import org.unibl.etf.gui.view.base.BaseController;
 
 import javafx.application.Application;
@@ -13,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage)  {
 		try {
-			BaseController.changeScene("/org/unibl/etf/gui/plants/view/BasesView.fxml", primaryStage);
+			BaseController.changeScene("/org/unibl/etf/gui/plants/view/DrawRegionsView.fxml", primaryStage);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println(e.getCause());
